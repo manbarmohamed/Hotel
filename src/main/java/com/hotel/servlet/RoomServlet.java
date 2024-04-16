f68a7ea21099bfb2c51efa04ade2f9cbc63f93c4
manbarmohamed <you@example.com>
2024-04-16 17:31:39 +0100

    connection

---
+++ src/main/java/com/hotel/servlet/RoomServlet.java
@@ -0,0 +1,37 @@
+package com.hotel.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hotel.DAO.RoomDAO;
+import com.hotel.modal.Room;
+
+
+@WebServlet("/RoomServlet")
+public class RoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/Room.jsp").forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    RoomDAO roomDAO = new RoomDAO();
+	    List<Room> rooms = roomDAO.selectAllRom();
+	    request.setAttribute("listrooms", rooms); 
+	    request.getRequestDispatcher("/Room.jsp").forward(request, response);
+	}
+
+
+	}
+
+
