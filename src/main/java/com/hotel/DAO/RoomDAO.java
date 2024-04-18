f5d7066d96196d9e0771754611c07e42094a8831
manbarmohamed <you@example.com>
2024-04-18 17:19:41 +0100

    realisé la methode cancel la reservation et change availability dans la table room

--- src/main/java/com/hotel/DAO/RoomDAO.java
+++ src/main/java/com/hotel/DAO/RoomDAO.java
@@ -1,5 +1,6 @@
 package com.hotel.DAO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.hotel.modal.Room;
@@ -7,5 +8,5 @@ import com.hotel.modal.Room;
 public interface RoomDAO {
 	
 	 List<Room> selectAllRom();
-	 boolean isReserved(int id_room);
+	 boolean isReserved(int id_room) throws SQLException;
 }
